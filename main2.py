793e48f21d682ccadaeac8b3c528af3619d1144b
Ivelin Peychev <peychev.vn@gmail.com>
2023-04-02 14:44:29 +0300

    Add more pages for the pdf file

--- main2.py
+++ main2.py
@@ -9,15 +9,18 @@ file = pd.read_csv('topics.csv')
 # We read the file row with pandas
 for index, row in file.iterrows():
 
-    # we take the number of the pdf based of the number placed in the document
-    for number in range(int(row['Pages'])):
-        # Check main.py for more information
-        pdf.add_page()
+    # Check main.py for more information
+    pdf.add_page()
 
-        pdf.set_font(family='Times', style='B', size=24)
-        pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1,)
-        pdf.line(10, 20, 200, 20)
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1,)
+    pdf.line(10, 20, 200, 20)
+
+    # we take the number of the pdf based of the number placed in the document and adding blank pages
+    # We take -1 because of the title page above
+    for number in range(int(row['Pages']) -1):
+        pdf.add_page()
 
 
 pdf.output("output.pdf")
